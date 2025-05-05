0a526283fc4c7e222ce555e9ff874df941e6bc51
abdlaziz15bnaame <abdlaziz15bnaame@gmail.com>
2025-05-05 16:53:40 +0200

    first commit

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -1,42 +1,48 @@
-
-import { Dispatch, SetStateAction } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Dispatch, SetStateAction, MouseEvent } from "react";
 
 type CellProps = {
-    id:number;
-    go:string;
+    id: number;
+    go: string;
     setGo: Dispatch<SetStateAction<string>>;
-    cells:string[];
+    cells: string[];
     setCells: Dispatch<SetStateAction<string[]>>;
     cell: string;
     winningMessage: string;
 };
+
 const Cell = ({ go, setGo, id, cells, setCells, cell, winningMessage }: CellProps) => {
 
-const handleClick = (e) => {
-    if (winningMessage) {
-        return;
-    }
-        const  notTaken = !cells[id]
-
-        if (notTaken) {
-        if(go === "circle") {
-            handleCellChange("circle")
-            setGo ("cross")
-        }else if(go === "cross") {
-            handleCellChange("cross")
-            setGo ("circle")
+    const handleClick = (e : MouseEvent<HTMLDivElement>) => {
+        if (winningMessage) {
+            return;
         }
-    }
-};
+
+        const notTaken = !cells[id]; 
+        if (notTaken) {
+            if (go === "circle") {
+                handleCellChange("circle");
+                setGo("cross");
+            } else if (go === "cross") {
+                handleCellChange("cross");
+                setGo("circle");
+            }
+        }
+    };
+
     const handleCellChange = (cellToChange: string) => {
-        let copyCells = [...cells]
-        copyCells[id] = cellToChange
-        setCells(copyCells)
-    }
-    return <div className="square"onClick={handleClick}>
-            <div className={cell}>{cell ? cell ==="circle" ? "o" :"X" : ""} </div>
-    </div>
-    
+        const copyCells = [...cells];
+        copyCells[id] = cellToChange;
+        setCells(copyCells);
+    };
+
+    return (
+        <div className="square" onClick={handleClick}>
+            <div className={cell}>
+                {cell ? (cell === "circle" ? "o" : "X") : ""}
+            </div>
+        </div>
+    );
 };
 
 export default Cell;
